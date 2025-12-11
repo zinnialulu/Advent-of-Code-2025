b9f74548cce500f617136b55f1337c74618a66d6
zinnialulu <49621708+zinnialulu@users.noreply.github.com>
2025-12-11 15:05:05 +0000

    day10 modified

--- Day10.py
+++ Day10.py
@@ -1,4 +1,6 @@
 #read the file
+import collections
+import datetime
 
 def target_transform(s:str) -> str:
     target=""
@@ -57,27 +59,31 @@ def joltage_transform(s:str) -> list:
     
     
 def shortest_path(joltage_input:list, available_edge:list,n:int) -> int:
-    start_node=[0 for i in range(n)] #start node in forme of [0,0,0,...]
+    start_node=tuple(0 for i in range(n)) #start node in forme of [0,0,0,...]
+    joltage_input = tuple(joltage_input)
     if start_node==joltage_input:
         return 0
     
     moves = [[int(j) for j in i] for i in available_edge] #transform each edge into a list
     
-    tot = [(start_node, 0)] # Stores (current_state_integer, paths_taken)
-    visited = [start_node]
+    tot = collections.deque([(start_node, 0)]) # Stores (current_state_integer, paths_taken)
+    visited = set([start_node])
 
     while tot:
         # Use pop(0) to remove the first element (the "front" of the tot)
-        current_state, paths = tot.pop(0) 
+        current_state, paths = tot.popleft() 
 
         for move in moves:
-            next_state = list(current_state[i]+move[i] for i in range(len(current_state)))
+            next_state = tuple(current_state[i]+move[i] for i in range(len(current_state)))
             
             if next_state == joltage_input:
+                print(f"BAR: {len(tot)=} {len(visited)=}")
                 return paths + 1
+            elif any(next_state[i] > joltage_input[i] for i in range(len(next_state))):
+                continue
 
             if next_state not in visited:
-                visited.append(next_state)
+                visited.add(next_state)
                 tot.append((next_state, paths + 1))
 
     return -1
@@ -86,7 +92,7 @@ with open('C:/Users/yingl/OneDrive/Documents/GitHub/Advent-of-Code-2025/day10.tx
     input=file.readlines()
     count=0
     count2=0
-    for line in input:
+    for idx, line in enumerate(input):
         line=line.rstrip()
         a=line.index(']')
         #print(a)
@@ -112,7 +118,9 @@ with open('C:/Users/yingl/OneDrive/Documents/GitHub/Advent-of-Code-2025/day10.tx
         
         min_path_2=shortest_path(joltage_target,edge_l,n_target)
         #print(min_path_2)
+        print(f"FOO: {datetime.datetime.now()} line={idx} {min_path_2=}")
         count2+=min_path_2
         #print(joltage)
+        #break
 print("Total count for part 1:", count)
 print("Total count for part 2:", count2)
