6c3325a188cb3cdf5ed63c4170275602d5c0050c
zinnialulu <49621708+zinnialulu@users.noreply.github.com>
2025-12-11 14:57:24 +0000

    day11 p2

--- Day11.py
+++ Day11.py
@@ -26,11 +26,41 @@ def find_all_path(t:dict, start:str, target:str,path=[]) -> list:
     return all_path
 
 
-print(len(find_all_path(tree,'you','out')))
+print("part 1:", len(find_all_path(tree,'you','out')))
 
-p2=find_all_path(tree,'svr','out')
-c2=0
-for i in p2:
-    if 'dac' in i and 'fft' in i:
-        c2+=1
-print(c2)
+###part 2 svr to out is too big
+
+#check svr to dac
+tree2={i:j for i,j in tree.items() if (i!='fft')and (i!='out')}
+svr_to_dac=find_all_path(tree2,'svr','dac')
+print("from svr to dac:",len(svr_to_dac))
+
+
+#check svr to fft
+tree2={i:j for i,j in tree.items() if (i!='dac')and (i!='out')}
+svr_to_fft=find_all_path(tree2,'svr','fft')
+print("from svr to fft:",len(svr_to_fft))
+
+
+#check between dac and fft
+tree2={i:j for i,j in tree.items() if (i!='svr')and (i!='out')}
+dac_to_fft=find_all_path(tree2,'dac','fft')
+fft_to_dac=find_all_path(tree2,'fft','dac')
+print("from dac to fft:",len(dac_to_fft))
+print("from fft to dac:",len(fft_to_dac))
+
+#check dac to out
+tree2={i:j for i,j in tree.items() if (i!='svr')and (i!='ffc')}
+dac_to_out=find_all_path(tree2,'dac','out')
+print("from dac to out:",len(dac_to_out))
+
+#check fft to out
+tree2={i:j for i,j in tree.items() if (i!='svr')and (i!='dac')}
+fft_to_out=find_all_path(tree2,'fft','out')
+print("from fft to out:",len(fft_to_out))
+
+#svr to dac to fft to out
+l1=len(svr_to_dac)*len(dac_to_fft)*len(fft_to_out)
+l2=len(svr_to_fft)*len(fft_to_dac)*len(dac_to_out)
+
+print(l1+l2)
